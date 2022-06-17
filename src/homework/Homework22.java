1d35afb76e63557ce917325d3c7e0f34a71de7d9
AndreiMitioglo <mitioandrew2@gmail.com>
2022-06-16 22:03:28 -0500

    Homework22

--- src/homework/Homework22.java
+++ src/homework/Homework22.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Homework22 {
  /*
-     Requirement:
+     Requirement: TASK 1
     -Create a method called fibonacciSeries1()
     -This method will take an int argument as n, and it will return n series of Fibonacci numbers as an int array.
 
@@ -29,7 +29,7 @@ public class Homework22 {
         int[] newArr = new int[n];
         for (int i = 1; i < n; i++) {
             newArr[0] = 0;
-            newArr[1]= 1;
+            newArr[1] = 1;
             newArr[i] = n2;
             n2 = n0 + n1;
             n0 = n1;
@@ -38,9 +38,9 @@ public class Homework22 {
         }
         return newArr;
 
-
+    }
     /*
-        Requirement:
+        Requirement: TASK 2
     -Create a method called fibonacciSeries2()
     -This method will take an int argument as n, and it will return the nth series of Fibonacci number as an int.
 
@@ -58,25 +58,159 @@ public class Homework22 {
 
      */
 
-    public  static int fibonacciSeries2(int n){
-      String fibonacci = "";
-        int n1=0, n2=1, n3;
-        for (int i=0; i<=n; i++){
+    public static int fibonacciSeries2(int n) {
+        int n0 = 0, n1 = 1, n2 = 0;
+        int[] newArr = new int[n];
+        for (int i = 1; i < n; i++) {
+            newArr[0] = 0;
+            newArr[1] = 1;
+            newArr[i] = n2;
+            n2 = n0 + n1;
+            n0 = n1;
+            n1 = n2;
 
-            n3= n1 + n2;
-            n2= n3;
-            n1= n2;
-          fibonacci+=n2 + ",";
         }
-        return fibonacci;
+        return newArr[n - 1];
     }
+
+
+
+    /*
+        Requirement: TASK 3
+    -Create a method called findUniques()
+    -This method will take 2 int array argument and it will return an int array which has only the unique values
+    from both given arrays.
+    NOTE: If both arrays are empty, then return an empty array.
+    NOTE: if one of the array is empty, then return unique values from the other array.
+
+    Test Data 1: [], []
+    Expected Result 1: []
+
+    Test Data 2: [], [1, 2, 3, 2]
+    Expected Result 2: [1, 2, 3]
+
+    Test Data 3: [1, 2, 3, 4], [3, 4, 5, 5]
+    Expected Result 3: [1, 2, 5]
+
+    Test Data 4: [8, 9], [9, 8, 9]
+    Expected Result 4: []
+
+     */
+
+    public static int[] findUniques(int[] a, int[] b) {
+        String str = "";
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if ((a[i] != b[j])&& !str.contains(a[i]+"")) str += a[i];
+            }
+        }
+        int[] c = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            c[i] =Integer.valueOf(i);
+
+        }
+        return c;
+    }
+
+
+        /*
+        Requirement: TASK 4
+    -Create a method called isPowerOf3()
+    -This method will take an int argument and it will return true if given
+    int argument is equal to 3 power of the X. Otherwise, it will return false.
+
+
+    Numbers that are power of 3 = 1, 3, 9, 27, 81, 243….
+
+    Test Data 1: 1
+    Expected Result 1: true
+
+    Test Data 2: 2
+    Expected Result 2: false
+
+    Test Data 3: 3
+    Expected Result 3: true
+
+    Test Data 4: 81
+    Expected Result 4: true
+
+     */
+public static boolean isPowerOf3(int n){
+    if (n==1) return true;
+    return n%3==0;
+
+}
+
+/*
+    Requirement: Task 5
+    -Create a method called firstDuplicate()
+    -This method will take an int array argument and it will
+    return an int which is the first duplicated number.
+    NOTE: All elements will be positive numbers.
+    NOTE: If there are no duplicates, then return -1
+    NOTE: If there are more than one duplicate, then return
+    the one for which second occurrence has the smallest
+    index.
+    Test Data 1: []
+    Expected Result 1: -1
+
+    Test Data 2: [1]
+    Expected Result 2: -1
+
+    Test Data 3: [1, 2, 2, 3]
+    Expected Result 3: 2
+
+    Test Data 4: [1,  2, 3, 3, 4, 1]
+    Expected Result 4: 3
+ */
+
+    public static int firstDuplicate(int[] a) {
+        String str = "";
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 1; j < a.length; j++) {
+                if (((a[i]==a[j]) && (!str.contains(a[j]+""))) && (a[j]>max)){
+                       str += a[j];
+                   }
+                max = a[j];
+                } break;
+            }
+
+
+       return Integer.parseInt(str);
+
+        }
+
+
+
+
+
 
 
     public static void main(String[] args) {
-
-       // System.out.println(fibonacciSeries1(5));
+        System.out.println(Arrays.toString(fibonacciSeries1(5)));
 
         System.out.println(fibonacciSeries2(5));
+        int[] a = new int[7];
+        int[] b = new int[4];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 2;
+        a[3] = 4;
+        a[4] = 4;
+        a[5] = 5;
+        a[6] = 4;
 
+        b[0] = 3;
+        b[1] = 4;
+        b[2] = 5;
+        b[3] = 5;
+
+            System.out.println(Arrays.toString(findUniques(a, b)));
+            // System.out.println(Arrays.toString(findUniques(a,b)));
+
+        System.out.println(isPowerOf3(28));
+
+        System.out.println(firstDuplicate(a));
+        }
     }
-}
